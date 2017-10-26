1b11d587704ffa87e9a2fc93c0a3013e197bcb0b
caoimheharvey <caoimhe.e.harvey@gmail.com>
2017-10-26 16:26:13 +0100

    testing/logging added

--- hashingTest.py
+++ hashingTest.py
@@ -6,12 +6,23 @@ to compare text based files for duplication system.
 """
 __author__ = "Caoimhe Harvey"
 
-input("Modifications for this test: ")
+testing = input("Modifications for this test: ")
 import methods
+
 file1_path = input("Please enter file path for the first file: ")
 file2_path = input("Please enter file path for the second file: ")
 
 print("\nHashing........\n")
 
-print("FILE1: {0}" .format(methods.hasht(file1_path)))
-print("FILE2: {0}" .format(methods.hasht(file2_path)))
+hash1 = methods.hasht(file1_path)
+print("FILE 1: " + hash1)
+hash2 = methods.hasht(file2_path)
+print("FILE 2: " + hash2)
+
+testingFile = open("./testingLogs/hashTestingTextFiles.txt", "a")
+testingFile.write("\n--------------------------------------------------\n\n")
+testingFile.write(testing + "\n")
+testingFile.write("\nFile 1: " + file1_path + "\t " + hash1)
+testingFile.write("\nFile 2: " + file2_path + "\t " + hash2)
+testingFile.write("\n\n--------------------------------------------------\n")
+testingFile.close()
